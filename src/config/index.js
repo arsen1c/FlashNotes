edd55486b7c6087026f1b864809c9d057886e6f0
Arsenic <vector2912@gmail.com>
2021-10-30 17:12:22 +0530

    fix: client link 🐛

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 const DEVELOPMENT = false;
 
 let server = DEVELOPMENT ? 'http://localhost:4000/api' : 'https://react-notes-api.vector2912.repl.co/api';
-let client = DEVELOPMENT ? 'http://localhost:3000' : 'https://flashnote.vercel.app';
+let client = DEVELOPMENT ? 'http://localhost:3000' : 'https://flashnotes.vercel.app';
 
 export {
   server,
