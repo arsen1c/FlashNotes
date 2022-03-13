6697468fa430cdbfbc0f479458bcf87e8d14f799
arsen1c <vector2912@gmail.com>
2022-03-13 16:57:20 +0530

    🎉fix: deployment settings

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
-const DEVELOPMENT = true;
+const DEVELOPMENT = false;
 
 let server = DEVELOPMENT ? 'http://localhost:4000/api' : 'https://flashnotes-api.up.railway.app/api';
 let client = DEVELOPMENT ? 'http://localhost:3000' : 'https://flashnotes.vercel.app';
