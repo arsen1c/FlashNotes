ecd218a74c9c17cf70be5408f0a0e11d53300434
arsen1c <vector2912@gmail.com>
2024-03-29 21:10:32 +0530

    fix: server url

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const DEVELOPMENT = false;
 
-let server = DEVELOPMENT ? 'http://localhost:4000/api' : 'https://react-notes-api.vector2912.repl.co/api';
+let server = DEVELOPMENT ? 'http://localhost:4000/api' : 'https://flashnotes.up.railway.app/api';
 let client = DEVELOPMENT ? 'http://localhost:3000' : 'https://flashnotes.vercel.app';
 
 export {
